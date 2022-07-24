916c2803b77f9a1d7cab0efe62275b67fa71aaf9
Keenan <keenanpaulmendiola@gmail.com>
2022-07-25 07:57:52 +0800

    Add sample scenarios for testing

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 import ParkingService from "./services/ParkingService.js";
-let numberOfEntryPoints = 3;
 
-let parkingService = new ParkingService(numberOfEntryPoints);
-
-parkingService.parkCar(2, "S", "ABC123");
-parkingService.viewParking();
-parkingService.parkCar(3, "M", "ABC124");
-parkingService.viewParking();
-parkingService.parkCar(1, "L", "ABC125");
-parkingService.viewParking();
-parkingService.parkCar(3, "S", "ABC126");
-parkingService.viewParking();
-parkingService.parkCar(2, "M", "ABC127");
-parkingService.viewParking();
-parkingService.parkCar(1, "L", "ABC128");
-parkingService.viewParking();
 // parkingService.unparkCar("ABC123");
 // parkingService.viewParking();
 // parkingService.unparkCar("ABC127");
@@ -39,4 +24,35 @@ parkingService.viewParking();
 // parkingService.viewParking();
 // parkingService.parkCar(3, "L", "ABC018");
 // parkingService.viewParking();
-console.log("");
+// console.log("");
+
+//Scenario 1
+//Assigning Car to Nearest parking slot
+// let numberOfEntryPoints = 3;
+
+// let parkingService = new ParkingService(numberOfEntryPoints);
+
+// parkingService.parkCar(2, "S", "ABC123");
+// parkingService.parkCar(3, "M", "ABC124");
+// parkingService.parkCar(1, "L", "ABC125");
+// parkingService.parkCar(3, "S", "ABC126");
+// parkingService.parkCar(2, "M", "ABC127");
+// parkingService.parkCar(1, "L", "ABC128");
+// parkingService.viewParking();
+
+//Scenario 2
+//Unparking car
+let numberOfEntryPoints = 3;
+
+let parkingService = new ParkingService(numberOfEntryPoints);
+
+parkingService.parkCar(2, "S", "ABC123");
+parkingService.parkCar(3, "M", "ABC124");
+parkingService.parkCar(1, "L", "ABC125");
+parkingService.parkCar(3, "S", "ABC126");
+parkingService.parkCar(2, "M", "ABC127");
+parkingService.parkCar(1, "L", "ABC128");
+parkingService.viewParking();
+parkingService.unparkCar("ABC128");
+parkingService.unparkCar("ABC123");
+parkingService.viewParking();
